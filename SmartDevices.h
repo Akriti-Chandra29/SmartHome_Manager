3526f0a35f3b6ff42b5e4c8d6fbf7375403825c0
sanskritikaushik21 <sanskritikaushik21@gmail.com>
2026-03-03 20:23:20 +0530

    Update SmartDevices.h

--- SmartDevices.h
+++ SmartDevices.h
@@ -1,1 +1,35 @@
+#ifndef SMARTDEVICES_H
+#define SMARTDEVICES_H
 
+class SmartDevice {
+protected:
+    string name;
+    int id;
+
+public:
+    bool status;
+
+  
+    SmartDevice(string deviceName, int deviceId) {
+        name = deviceName;
+        id = deviceId;
+        status = false;
+    }
+
+  
+    virtual ~SmartDevice() {}
+
+    
+    virtual void showAction() = 0;
+
+    string getName() { return name; }
+    int getId() { return id; }
+
+    void changeStatus() {
+        if (status == true) {
+            status = false;
+        } else {
+            status = true;
+        }
+    } 
+}
