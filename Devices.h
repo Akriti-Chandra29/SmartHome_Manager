622fd4b7cb2a0253edbe77dd8b2227bac0561627
Akriti-Chandra29 <akritichandra29@gmail.com>
2026-03-04 00:08:15 +0530

    Syntax error

--- Devices.h
+++ Devices.h
@@ -1,41 +1,42 @@
-
 #ifndef DEVICES_H
 #define DEVICES_H
 
 #include "SmartDevices.h"
 #include <string>
 
+// 1. Smart Light
 class SmartLight : public SmartDevice {
 public:
-    SmartLight(string n, int i) : SmartDevice(n, i) {}
+    SmartLight(std::string n, int i) : SmartDevice(n, i) {}
     void showAction() override; 
 };
 
-
+// 2. Smart Fan
 class SmartFan : public SmartDevice {
 public:
-    SmartFan(string n, int i) : SmartDevice(n, i) {}
+    SmartFan(std::string n, int i) : SmartDevice(n, i) {}
     void showAction() override;
 };
 
-
+// 3. Smart AC
 class SmartAC : public SmartDevice {
 public:
-    SmartAC(string n, int i) : SmartDevice(n, i) {}
+    SmartAC(std::string n, int i) : SmartDevice(n, i) {}
     void showAction() override;
 };
 
-
+// 4. Smart Camera
 class SmartCamera : public SmartDevice {
 public:
-    SmartCamera(string n, int i) : SmartDevice(n, i) {}
+    SmartCamera(std::string n, int i) : SmartDevice(n, i) {}
     void showAction() override;
 };
 
-
+// 5. Smart Heater
 class SmartHeater : public SmartDevice {
 public:
-    SmartHeater(string n, int i) : SmartDevice(n, i) {}
+    SmartHeater(std::string n, int i) : SmartDevice(n, i) {}
     void showAction() override;
 };
-#endif
+
+#endif 
