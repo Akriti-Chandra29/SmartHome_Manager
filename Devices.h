c6134ac17b2957d91d8864367fcef56b66f56838
Harshit Negi <109102106+aaalaskapedh@users.noreply.github.com>
2026-03-03 23:45:05 +0530

    Add SmartDevice subclasses for smart home devices

--- Devices.h
+++ Devices.h
@@ -1,1 +1,39 @@
 
+#ifndef DEVICES_H
+#define DEVICES_H
+
+#include "SmartDevices.h"
+
+class SmartLight : public SmartDevice {
+public:
+    SmartLight(string n, int i) : SmartDevice(n, i) {}
+    void showAction() override; 
+};
+
+
+class SmartFan : public SmartDevice {
+public:
+    SmartFan(string n, int i) : SmartDevice(n, i) {}
+    void showAction() override;
+};
+
+
+class SmartAC : public SmartDevice {
+public:
+    SmartAC(string n, int i) : SmartDevice(n, i) {}
+    void showAction() override;
+};
+
+
+class SmartCamera : public SmartDevice {
+public:
+    SmartCamera(string n, int i) : SmartDevice(n, i) {}
+    void showAction() override;
+};
+
+
+class SmartHeater : public SmartDevice {
+public:
+    SmartHeater(string n, int i) : SmartDevice(n, i) {}
+    void showAction() override;
+};
