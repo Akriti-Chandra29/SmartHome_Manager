08c0108c7bb6e10f2e980b9bd5136078e642219d
sanskritikaushik21 <sanskritikaushik21@gmail.com>
2026-04-06 22:27:55 +0530

    Update device.h

--- device.h
+++ device.h
@@ -1,1 +1,20 @@
-//Base abstract class for all devices in the smart home
+#include <iostream>
+using namespace std;
+
+class Device {
+protected:
+    string name;
+    bool status;
+
+public:
+    Device(string n);
+
+    virtual void turnOn() = 0;
+    virtual void turnOff() = 0;
+    virtual void showStatus() = 0;
+
+    string getName();
+    bool getStatus();
+
+    virtual ~Device() {}
+};
