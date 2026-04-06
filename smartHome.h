863b08bbcc913a0a2e84ee52ca58bdf70fd0debe
AkritiChandra29 <akritichandra29@gmail.com>
2026-04-06 21:51:15 +0530

    Add SmartHome class to manage rooms

--- smartHome.h
+++ smartHome.h
@@ -1,1 +1,13 @@
 //Manages all rooms
+#include "room.h"
+class SmartHome {
+private:
+    Room* rooms[5];  
+    int count;       
+
+public:
+    SmartHome();
+    void addRoom(Room* r);
+    void showRooms();
+};
+
