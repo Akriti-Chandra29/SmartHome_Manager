a4a0612d58250264e378a7ccb476e6a887ac60c7
AkritiChandra29 <akritichandra29@gmail.com>
2026-04-06 21:53:25 +0530

    Add SmartHome class with room management functions
    Implement SmartHome class to manage rooms and devices.

--- smartHome.cpp
+++ smartHome.cpp
@@ -1,1 +1,20 @@
 //Adds and displays all rooms and their devices
+#include "smartHome.h"
+SmartHome::SmartHome() {
+    count = 0;
+}
+void SmartHome::addRoom(Room* r) {
+    if (count < 5) {
+        rooms[count] = r;
+        count++;
+    } else {
+        cout << "Cannot add more rooms";
+    }
+}
+void SmartHome::showRooms() {
+    cout <<Smart Home Status";
+
+    for (int i = 0; i < count; i++) {
+        rooms[i]->showDevices();
+    }
+}
