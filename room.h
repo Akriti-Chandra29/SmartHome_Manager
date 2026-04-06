57839b1d9344ebafc518236e11ada13d32748258
Harshit Negi <109102106+aaalaskapedh@users.noreply.github.com>
2026-04-06 22:21:38 +0530

    Update room.h

--- room.h
+++ room.h
@@ -1,1 +1,20 @@
-//Room class manages multiple devices inside a room
+#include "device.h"
+
+class Room {
+private:
+    string roomName;   
+
+    Device* devices[10]; 
+    int count;            
+
+public:
+    Room(string name);
+    void addDevice(Device* d);
+
+    // display all devices
+    void showDevices();
+
+    // function overloading
+    void controlDevice(string name);            
+    void controlDevice(string name, int val);  
+};//Room class manages multiple devices inside a room
